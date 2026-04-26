e0c91b84ddf48311723344f0af98ea4899c647c0
Zeb <zeb.cappaert@gmail.com>
2026-04-26 15:31:05 +0200

    Updated om subdomein te laten werken

--- backend/src/main/java/softball/app/config/WebConfig.java
+++ backend/src/main/java/softball/app/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://softballu23.eu", "https://softballu23.eu")
+                .allowedOriginPatterns("http://softballu23.eu", "https://softballu23.eu", "http://test.softballu23.eu",
+                        "https://test.softballu23.eu")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
