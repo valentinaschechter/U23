a954cefb26cc89fd853801da66c9dff2615f86e1
Zeb <zeb.cappaert@gmail.com>
2026-05-15 10:36:25 +0200

    Debug POST 404 api/reflection/new

--- backend/src/main/java/softball/app/AppApplication.java
+++ backend/src/main/java/softball/app/AppApplication.java
@@ -62,7 +62,7 @@ public class AppApplication {
 						.requestMatchers(HttpMethod.DELETE, "/api/posts/**").hasAnyAuthority("COACH")
 						.requestMatchers(HttpMethod.POST, "/api/posts/**").hasAnyAuthority("COACH")
 						.requestMatchers(HttpMethod.POST, "/api/posts/**").hasAnyAuthority("COACH")
-						.requestMatchers(HttpMethod.POST, "/api/reflections").permitAll()
+						.requestMatchers(HttpMethod.POST, "/api/reflections/new").hasAnyAuthority("PLAYER")
 						.requestMatchers("/api/reflections/summary").hasAnyAuthority("COACH")
 						.requestMatchers("/api/reflections/player/**").hasAnyAuthority("COACH")
 						.anyRequest().permitAll())
