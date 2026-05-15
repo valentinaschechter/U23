433f6bf2a5aeb3bc4d92a2e5b500a74181f9ba06
Zeb <zeb.cappaert@gmail.com>
2026-05-15 11:13:48 +0200

    Deleted /api prefix

--- backend/src/main/java/softball/app/controller/ReflectionController.java
+++ backend/src/main/java/softball/app/controller/ReflectionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +18,8 @@ import softball.app.repository.ReflectionRepository;
 import softball.app.services.ReflectionService;
 
 @RestController
-@RequestMapping("/api/reflections")
+@RequestMapping("/reflections")
+@CrossOrigin("https://test.softballu23.eu")
 public class ReflectionController {
 
     private final ReflectionService reflectionService;
